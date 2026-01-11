4a2b167a7baf29809a8da0a2f7482bb4b98d899c
Hasanli <a.hasanli@epartment.nl>
2026-01-11 19:34:17 +0100

    revert

--- run-test.ps1
+++ run-test.ps1
@@ -1,26 +1,3 @@
-# nb - no build
-# d - compose down
-# f - force remove
-param(
-    [switch]$nb,
-    [switch]$d,
-    [switch]$f
-)
-if (!$d.IsPresent -and !$f.IsPresent) {
-    if (!$nb.IsPresent) {
-        Write-Host -ForegroundColor Yellow "Running docker compose build!"
-        docker compose build
-    }
-    Write-Host -ForegroundColor Yellow "Running docker compose up!"
-    docker compose -f .\docker-compose.local.test.yml up -d --build
-}
-else {
-    if ($f.IsPresent) {
-        Write-Host -ForegroundColor Red "Forcing deletion of containers!"
-        docker compose -f .\docker-compose.local.test.yml down --rmi all -v --remove-orphans
-    }
-    else {
-        Write-Host -ForegroundColor Red "Running docker compose down!"
-        docker compose -f .\docker-compose.local.test.yml down
-    }
-}
+#!/bin/bash
+docker compose -f docker-compose.local.test.yml down --rmi all -v --remove-orphans
+docker compose -f docker-compose.local.test.yml up -d
